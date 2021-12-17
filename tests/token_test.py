9c2c303eebb7fa6b6473df2666100ee693169f71
jonathan6620 <34371202+jonathan6620@users.noreply.github.com>
2021-12-17 21:05:51 +0000

    cleaned up negative tests

--- tests/token_test.py
+++ tests/token_test.py
@@ -9,7 +9,7 @@ amount = 1000
 
 @pytest.fixture(scope="module", autouse=True)
 def token(MobixToken, accounts):
-    yield MobixToken.deploy("MOBIX", "MBX", 9, totalSupply, {'from': accounts[0]})
+    yield MobixToken.deploy("MOBIX", "MOBX", 9, totalSupply, {'from': accounts[0]})
 
 @pytest.fixture(autouse=True)
 def isolate(fn_isolation):
@@ -21,7 +21,7 @@ def test_initiation(token, accounts):
     assert token.totalSupply() == totalSupply
     assert token.name() == "MOBIX"
     assert token.decimals() == 9 
-    assert token.symbol() == "MBX"
+    assert token.symbol() == "MOBX"
 
 def test_transfer(token, accounts):
     tx = token.transfer(accounts[1], amount, {'from': accounts[0]})
@@ -32,7 +32,6 @@ def test_transfer(token, accounts):
 def test_transfer_fail(token, accounts):
     with brownie.reverts():
         tx = token.transfer(accounts[0], amount, {'from': accounts[1]})
-        assert tx.return_value == False
 
 def test_approve(token, accounts):
     tx = token.approve(accounts[1], amount, {'from': accounts[0]})
@@ -40,15 +39,14 @@ def test_approve(token, accounts):
     assert token.allowance(accounts[0], accounts[1]) == amount
     with brownie.reverts():
         tx = token.transferFrom(accounts[0], accounts[2], amount+1, {'from': accounts[1]})
-        assert tx.return_value == False
     tx = token.transferFrom(accounts[0], accounts[2], amount, {'from': accounts[1]})
     assert tx.return_value == True
 
 def test_change_allowance(token, accounts):
     with brownie.reverts():
-        assert not token.decreaseAllowance(accounts[1], amount, {'from': accounts[0]}).return_value
+        token.decreaseAllowance(accounts[1], amount, {'from': accounts[0]})
 
-    assert token.increaseAllowance(accounts[1], amount, {'from': accounts[0]}).return_value 
+    assert token.increaseAllowance(accounts[1], amount, {'from': accounts[0]}).return_value
     assert token.allowance(accounts[0], accounts[1]) == amount
     assert token.decreaseAllowance(accounts[1], 1, {'from': accounts[0]}).return_value 
     assert token.allowance(accounts[0], accounts[1]) == amount-1
@@ -60,4 +58,3 @@ def test_approve_revoke(token, accounts):
     assert token.allowance(accounts[0], accounts[1]) == 0
     with brownie.reverts():
         tx = token.transferFrom(accounts[0], accounts[2], 1, {'from': accounts[1]})
-        assert tx.return_value == False
